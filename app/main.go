7b9b96bb1c22c8b4b482704d887aab8af93d68c6
jobint001 <jobint911@gmail.com>
2026-06-16 23:53:03 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -10,7 +10,7 @@ var _ = fmt.Print
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	 fmt.Print("$ ")
-	 var i int
+	 var i string
 	 _, err := fmt.Scanln(&i)
 	 if err != nil {
 		 fmt.Println("Error reading input:", err)
