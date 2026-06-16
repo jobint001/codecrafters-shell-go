ef5a1c4ffefe9e056e4a4e911528b12de66bce99
jobint001 <jobint911@gmail.com>
2026-06-16 23:52:21 +0530

    codecrafters submit [skip ci]

--- app/main.go
+++ app/main.go
@@ -10,4 +10,11 @@ var _ = fmt.Print
 func main() {
 	// TODO: Uncomment the code below to pass the first stage
 	 fmt.Print("$ ")
+	 var i int
+	 _, err := fmt.Scanln(&i)
+	 if err != nil {
+		 fmt.Println("Error reading input:", err)
+		 return
+	 }
+	fmt.Printf("%v: command not found",i)
 }
